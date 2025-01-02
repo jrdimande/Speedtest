0fc9f90f12858a6b76d7350f44cd1bc973ecc0f6
jrdimande <timoteojrdimande@gmail.com>
2025-01-02 00:18:27 +0000

    ...

---
+++ src/Speedtest.py
@@ -0,0 +1,42 @@
+import speedtest
+import datetime
+import time
+import json
+
+
+filename = '/Data.json'
+
+print("Executando o teste...")
+
+
+
+st = speedtest.Speedtest()
+
+st.get_best_server()
+
+download = st.download() / 1000000
+upload = st.upload() / 1000000
+ping = st.results.ping
+
+date = str(datetime.date.today())
+current_time = time.localtime()
+formatted_time = str(time.strftime("%H:%M:%S", current_time))
+
+
+speed = {"date" : date, "time" : formatted_time, "download" : download, "upload" : upload, "ping" : ping}
+
+with open(filename) as f:
+    content = json.load(f)
+
+content.append(speed)
+
+
+with open(filename, 'w') as f:
+    json.dump(content, f, indent=4)
+
+
+
+
+print(f"Velocidade de Download: {download: .2f} Mbps")
+print(f"Velocidade de Upload: {upload: .2f} Mbps")
+print(f"Ping: {ping} ms")
---
+++ src/speed_analyzer.py
@@ -0,0 +1,46 @@
+import json
+
+filename = 'files/Data.json'
+filename1 = '/home/vboxuser/PycharmProject/Speed Test/relatorio.txt'
+
+with open(filename) as f:
+    content = json.load(f)
+
+# soma
+sum_download = 0
+sum_upload = 0
+sum_ping = 0
+
+# ultimos 3 elementos da lista
+last_three = content[-3:]
+
+# datas
+start = last_three[0]["date"]
+end = last_three[-1]["date"]
+
+# somando
+for down_up in last_three:
+    sum_download += down_up["download"]
+    sum_upload += down_up["upload"]
+    sum_ping += down_up["ping"]
+
+# Calcular as médias
+down_avarage = sum_download / len(last_three)
+up_avarage = sum_upload / len(last_three)
+ping_avarage = sum_ping / len(last_three)
+
+# status
+down_status = (down_avarage >= 4 and down_avarage <= 10)
+up_status = (up_avarage >= 1 and up_avarage <= 3)
+ping_status = (ping_avarage <= 100)
+
+# Criar relatorio
+text = (f'\nAvarage Download: {down_avarage: .2f} Mbps  Stable = {down_status}\n'
+        f'Avarage Upload: {up_avarage: .2f} Mbps  Stable = {up_status}\n'
+        f'Avarage Ping: {ping_avarage: .2f} ms  Stable = {ping_status}\nStart: {start} End: {end}\n')
+
+
+with open(filename1, 'a') as f:
+    f.write(text)
+
+print(f"Report available at the file {filename1}")
